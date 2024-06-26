d12207d4f4a24dd23f0e91cb1a508bff584deb14
ZeroRickMore <riccardo.moretti.gg@gmail.com>
2024-06-26 16:45:15 +0200

    Implement elencoEditori

--- siw-variant/src/main/java/it/uniroma3/siw/controller/EditoreController.java
+++ siw-variant/src/main/java/it/uniroma3/siw/controller/EditoreController.java
@@ -2,7 +2,10 @@ package it.uniroma3.siw.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import it.uniroma3.siw.model.Editore;
 import it.uniroma3.siw.service.EditoreService;
 
 @Controller
@@ -18,11 +21,24 @@ public class EditoreController {
 	/*##############################################################*/
 	/*###########################METHODS############################*/
 	/*##############################################################*/
+//======================================================================\\
 	
 	/*##############################################################*/
-	/*########################/elencoEditori########################*/
+	/*###########################/Editori###########################*/
 	/*##############################################################*/
 	
+	@GetMapping("/elencoEditori")
+	public String showElencoEditori(Model model) {
+		Iterable<Editore> allEditori = this.editoreService.findAll();
+		model.addAttribute("allEditori", allEditori);
+		return "elencoEditori.html";
+	}
+	
+	@GetMapping("/editore/{id}")
+	public String showEditore(Model model) {
+		
+		return "editore.html";
+	}
 	
 	
 }
---
+++ siw-variant/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO editore (id, nazione, nome) VALUES(nextval('editore_seq'), 'Italia', 'J-POP')
+INSERT INTO editore (id, nazione, nome) VALUES(nextval('editore_seq'), 'Italia', 'Planet Manga')
+INSERT INTO editore (id, nazione, nome) VALUES(nextval('editore_seq'), 'Italia', 'Star Comics')
