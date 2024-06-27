6f9b409affd9f710ade46b448c653044f410b6cc
livingmeme03 <maria.lazzaro@hotmail.com>
2024-06-27 15:07:40 +0200

    Add protection from evildoers
    path traversal protection added

--- siw-variant/src/main/java/it/uniroma3/siw/controller/validation/EditoreValidator.java
+++ siw-variant/src/main/java/it/uniroma3/siw/controller/validation/EditoreValidator.java
@@ -10,28 +10,32 @@ import it.uniroma3.siw.service.EditoreService;
 
 @Component
 public class EditoreValidator implements Validator{
-	
+
 	@Autowired
 	private EditoreService editoreService;
 
-	
+
 
 	@Override
 	public void validate(Object o, Errors errors) {
-		
+
 		Editore editore = (Editore) o;
+		String pathImg = editore.getPathImmagine();
+		if(pathImg !=null && pathImg.contains("../")) {
+			errors.reject("aiuto.cihackerano.pathtraversal");
+		}
 		if(editore.getNome()!=null && editore.getNazione()!=null 
 				&& this.editoreService.existsByNomeAndNazione(editore.getNome(), editore.getNazione())) {
 			errors.reject("editore.duplicato");
 		}
-		
+
 	}
-	
+
 	@Override
 	public boolean supports(Class<?> aClass) {
 		return Editore.class.equals(aClass);
 	}
-	
-	
+
+
 
 }
