63811fa370019cfa05c4bf4610e2467741d7c1bf
ZeroRickMore <riccardo.moretti.gg@gmail.com>
2024-07-02 01:54:06 +0200

    Make it so that login doesnt always redirect to /

--- siw-variant/src/main/java/it/uniroma3/siw/authentication/AuthConfiguration.java
+++ siw-variant/src/main/java/it/uniroma3/siw/authentication/AuthConfiguration.java
@@ -87,7 +87,7 @@ public class AuthConfiguration {
 	  .and().formLogin()
 	  .loginPage("/login")
 	  .permitAll()
-	  .defaultSuccessUrl("/", true) //TODO: remove
+	  .defaultSuccessUrl("/")
 	  .failureUrl("/login?error=true")
 	  // LOGOUT: qui definiamo il logout
 	  .and()
